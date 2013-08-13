9fe990706600f25da1ac16a5ca8befd09cbc4f7e
Alison Major <alison.major@gmail.com>
2013-08-13 14:22:47 -0400

    Update copywrite on footer

--- public/_includes/_footer.php
+++ public/_includes/_footer.php
@@ -1,6 +1,6 @@
   </div>
   <div id="fineprint">
-    <p>&copy;2012 North Holland Reformed Church - Building Vision Task Force &#8226; <a href="sitemap.php">Sitemap</a></p>
+    <p>&copy;2012-2013 North Holland Reformed Church - Building Vision Task Force &#8226; <a href="sitemap.php">Sitemap</a></p>
   </div>
   </body>
 </html>
