7c52c43d7adcf1245cccdd03f008998bef7ea17b
Alison Major <alison.major@gmail.com>
2013-08-13 14:40:30 -0400

    Add link and page to start Phase 2!

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <h1 id="h1_index">Building Vision Task Force</h1>
     <h2>Phases</h2>
     <p><a href="phase1.php">Phase 1</a> - <em>Completed August 2012</em></p>
-    <p>Phase 2</p>
+    <p><a href="phase2.php">Phase 2</a> - <em>In Progress</em></p>
 
 <!-- BEGIN PAGE FOOTER HERE -->
 <?php include '_includes/_footer.php'; ?>
---
+++ public/phase2.php
@@ -0,0 +1,10 @@
+<!-- BEGIN PAGE HEADER HERE -->
+<?php include '_includes/_header.php'; ?>
+<!-- END PAGE HEADER HERE -->
+
+<h1>Phase 2</h1>
+<p>Phase 2 notes will start here!</p>
+
+<!-- BEGIN PAGE FOOTER HERE -->
+<?php include '_includes/_footer.php'; ?>
+<!-- END PAGE FOOTER HERE -->
