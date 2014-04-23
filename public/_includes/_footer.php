e97cc9229b9f15de1bc53956bf39eb673bfe1444
Alison Major <alison.major@gmail.com>
2014-04-22 22:10:19 -0400

    Switch from php to html
    Prep for making into a gh-pages branch

--- public/_includes/_footer.php
+++
@@ -1,6 +0,0 @@
-  </div>
-  <div id="fineprint">
-    <p>&copy;2012-2013 North Holland Reformed Church - Building Vision Task Force &#8226; <a href="sitemap.php">Sitemap</a></p>
-  </div>
-  </body>
-</html>
